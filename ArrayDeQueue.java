e6b61f6ac19eed8514e9248417fd6292dc053255
23CSADEEPAKB <139941497+23CSADEEPAKB@users.noreply.github.com>
2025-03-05 10:25:02 +0000

    submit

--- ArrayDeQueue.java
+++ ArrayDeQueue.java
@@ -10,7 +10,7 @@ queue = new int[capacity];
 }
 public int size()
 {
-    return (rear+1);
+ return (rear+1);
 }
 public void enQueue(int data)
 {
@@ -24,46 +24,71 @@ queue[++rear] = data;
 }
 public boolean isFull()
 {
-    return (size()==capacity);
-}
-
+ return (size()==capacity);
+ }
 public int deQueue()
 {
 // TYPE YOUR CODE
+int data=-1;
+if (isEmpty())
+{
+System.out.println("Queue is Underflow. No elements to be deleted in Empty Queue");
 }
-
+else
+{
+front++;
+data = queue[front];
+}
+return data;
+}
 public boolean isEmpty()
 {
-    // TYPE YOUR CODE
+ return (front==rear);
 }
 public void display()
 {
-    if(isEmpty())
-    System.out.println("Queue is Underflow. No elements to display in Empty Queue");
-    else
-    {
-    System.out.println("Queue Elements");
-    for(int i=front+1;i<=rear;i++)
-    System.out.print(queue[i]+"\t");
-    System.out.print("\n");
-    }
+ if(isEmpty())
+ System.out.println("Queue is Underflow. No elements to display in Empty Queue");
+ else
+ {
+ System.out.println("Queue Elements");
+ for(int i=front+1;i<=rear;i++)
+ System.out.print(queue[i]+"\t");
+ System.out.print("\n");
+ }
 }
 public static void main(String[] args)
 {
-     int data,n;
-    char ch;
-   
-    Scanner sc=new Scanner(System.in);
-     System.out.println("DeQueue Using Array");
-    System.out.println("Enter the number of elements to be inserted:");
+ int data,n;
+ char ch;
+ 
+ Scanner sc=new Scanner(System.in);
+ System.out.println("DeQueue Using Array");
+ System.out.println("Enter the number of elements to be inserted:");
 //TYPE YOUR CODE HERE
-     System.out.println("Queue Using Array created with size "+n);
-    q.display();
-    System.out.println("Do you want to Dequeue the Queue(y/n):");
+ n=sc.nextInt();
+ ArrayDeQueue q=new ArrayDeQueue(n); 
+ for(int i=1;i<=n;i++)
+ q.enQueue(i);
+ System.out.println("Queue Using Array created with size "+n);
+ q.display();
+ System.out.println("Do you want to Dequeue the Queue(y/n):");
 //TYPE YOUR CODE HERE
-    
-   }
+ ch=sc.next().charAt(0);
+ if(ch=='y')
+ {
+ do{
+ data=q.deQueue();
+ if(data!=-1)
+ {
+ System.out.println("Deleted Element is:"+data);
+ System.out.println("After Dequeue");
+ q.display();
+ }
+ System.out.println("Do you want to Dequeue the Queue(y/n):");
+ ch=sc.next().charAt(0);
+ 
+ } while(ch!='n');
+ }
+ }
 }
-
-
-
